473f644031cdf68fab794dd33e29c57a3b8ca501
arweber1 <arweber_93@hotmail.com>
2014-02-11 15:11:49 -0700

    Added class files

---
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/Thread.java
@@ -0,0 +1,24 @@
+package com.CMPUT301W14T13.gpscommentlogger;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import android.location.Location;
+
+
+
+public class Thread
+{
+
+	private String title;
+	private Date freshness;
+	private Comment rootComment;
+	private HashMap <Integer,Comment> comments
+	private Location GPS_location
+	
+	
+	
+	
+	
+	
+}
---
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/Vote.java
@@ -0,0 +1,15 @@
+package com.CMPUT301W14T13.gpscommentlogger;
+
+import java.util.Date;
+
+
+
+public class Vote
+{
+	private int value;
+	private Date timestamp;
+	private String androidID;
+		
+	
+	
+}
