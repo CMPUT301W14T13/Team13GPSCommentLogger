45b146edfb6ba159f59a7a87a05602823c523660
kokkenshodai <kyomaru@ualberta.ca>
2014-02-12 13:04:57 -0700

    ClientServerSystem

--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/ServerDispatcher.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/ServerDispatcher.java
@@ -1,7 +1,20 @@
 package com.CMPUT301W14T13.gpscommentlogger.controller;
 
+import com.CMPUT301W14T13.gpscommentlogger.model.ServerTask;
+
 
 public class ServerDispatcher
 {
+	private ServerController server;
+
+	public ServerDispatcher(ServerController server)
+	{
+		this.server = server;
+	}
+	
+	public void dispatch(ServerTask task)
+	{
+		server.addTask(task);
+	}
 
 }
---
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/model/ClientResult.java
@@ -0,0 +1,7 @@
+package com.CMPUT301W14T13.gpscommentlogger.model;
+
+
+public class ClientResult implements Result
+{
+
+}
