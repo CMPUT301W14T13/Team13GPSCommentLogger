987cff60ff28f8da8f321f7601db050c849207aa
Michael Nicholson <nicholson.michael.john@gmail.com>
2014-02-16 00:09:53 -0700

    Added non-compiling popularity test for comments in a thread.

--- GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/sortComments/SortCommentsByPopularityTests.java
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/sortComments/SortCommentsByPopularityTests.java
@@ -1,5 +1,6 @@
 package com.CMPUT301W14T13.gpscommentloggertests;
 import com.CMPUT301W14T13.gpscommentlogger.model.Comment;
+import com.CMPUT301W14T13.gpscommentlogger.model.CommentThread;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -13,8 +14,42 @@ import android.test.ActivityInstrumentationTestCase2;
     }
     
 
+    /* need to make a comment thread containing my comments 
+     then check to ensure that the index of the comments within
+     the array list are ordered correctly
+     */
     public void testSortCommentsByPopularity()
     {
+	Intent intent = new Intent();
+	setActivityIntent(intent);
+	DebugActivity activity = getActivity();
+
+	assertNotNull(activity);
+
+	/* Make a thread to contain the comments */
+	CommentThread thread = new CommentThread();
+
+	/* make two comments and set their vote counts */
+	Comment comment_1 = new Comment();
+	Comment comment_2 = new Comment();
+
+	/* set the vote count of each */
+	comment_1.setVoteCount(1);
+	comment_2.setVoteCount(0);
+
+	/* add the comments to the thread */
+	thread.addComment(comment_1);
+	thread.addComment(comment_2);
+
+	thread.sortByPopularity();
+
+	/* get the votes of the top two comments*/
+	Integer v0 = thread.get(0).getVoteCount();
+	Integer v1 = thread.get(1).getVoteCount();
+
+	/* check the comment order */
+	assertTrue("failure - comment 1 should preceed comment 2", v0 >= v1);
+  
 
     }
 
