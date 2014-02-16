7315cb1b2de9108ba9d7f7d201596eb0fbd32a8d
Michael Nicholson <nicholson.michael.john@gmail.com>
2014-02-15 23:18:57 -0700

    Changed my mind and went back to having one file/use case.

---
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/sortComments/SortCommentsByPopularityTests.java
@@ -0,0 +1,27 @@
+package com.CMPUT301W14T13.gpscommentloggertests;
+import com.CMPUT301W14T13.gpscommentlogger.model.Comment;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+
+@SuppressLint("NewApi")
+    public class SortCommentsByPopularityTests extends ActivityInstrumentationTestCase2<DebugActivity> {
+
+    public SortCommentsByPopularityTests() {
+	super(DebugActivity.class);
+    }
+    
+
+    public void testSortCommentsByPopularity()
+    {
+
+    }
+
+
+    public void testSortCommentThreadsByPopularity()
+    {
+
+    }
+
+}
