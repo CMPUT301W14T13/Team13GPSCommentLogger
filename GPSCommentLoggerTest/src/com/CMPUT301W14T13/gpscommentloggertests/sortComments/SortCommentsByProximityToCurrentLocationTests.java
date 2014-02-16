c5b9f27d8601050183009d92d889facdc2aebe8e
Michael Nicholson <nicholson.michael.john@gmail.com>
2014-02-16 00:53:51 -0700

    Pushing before finishing all the tests

--- GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/sortComments/SortCommentsByProximityToCurrentLocationTests.java
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/sortComments/SortCommentsByProximityToCurrentLocationTests.java
@@ -6,15 +6,40 @@ import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 
 @SuppressLint("NewApi")
-    public class SortingTestCases extends ActivityInstrumentationTestCase2<DebugActivity> {
+    public class SortCommentsByProximityToCurrentLocationTests extends ActivityInstrumentationTestCase2<DebugActivity> {
 
-    public SortingTestCases() {
+    public SortCommentsByProximityToCurrentLocationTests() {
 	super(DebugActivity.class);
     }
     
-    public void testSort()
+
+    /* need to make a comment thread containing my comments 
+     then check to ensure that the index of the comments within
+     the array list are ordered correctly
+     */
+    public void testSortCommentsByProximityToCurrent()
     {
+	Intent intent = new Intent();
+	setActivityIntent(intent);
+	DebugActivity activity = getActivity();
+
+	assertNotNull(activity);
+
+	/* Make a thread to contain the comments */
+	CommentThread thread = new CommentThread();
+
+	/* make two comments and set their locations */
+	Comment comment_1 = new Comment();
+	Comment comment_2 = new Comment();
+
+	/* add the comments to the thread */
+	thread.addComment(comment_1);
+	thread.addComment(comment_2);
+
+	thread.sortByProximity();
+
+	/* finish the rest of the test */
+	
 
     }
-
 }
