5ea49de9847906c783df6e8056f8e52967aabacf
Navjeet Dhaliwal <navjeet.d@gmail.com>
2014-02-16 01:01:21 -0700

    made DownloadCommentsTest.java with some empty tests

---
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/DownloadComments/DownloadCommentsTest.java
@@ -0,0 +1,27 @@
+package com.CMPUT301W14T13.gpscommentloggertests.DownloadComments;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class DownloadCommentsTest extends ActivityInstrumentationTestCase2<DebugActivity> {
+
+	public DownloadCommentsTest(String name) {
+		super(DebugActivity.class);
+		
+	}
+	//Use Case 4
+	public void testDownloadComments(){
+		
+		Viewable comment = new Comment();
+		//save the comment to file
+		//check file for this comment
+		
+	}
+	//Use Case 4.1
+	public void testSetAsFavorite() {
+		
+	}
+	//Use Case 4.1.1
+	public void testUpdateFavorite() {
+		
+	}
+}
