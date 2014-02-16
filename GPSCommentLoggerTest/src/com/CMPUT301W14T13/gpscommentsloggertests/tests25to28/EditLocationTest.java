f507cd49b5a1c0b1bdb440eeeee0f7ad6f72269b
Monir Imamverdi <monir.imamverdi@gmail.com>
2014-02-16 10:06:46 -0700

    2.6 test started, 2.7 test complete

---
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentsloggertests/tests25to28/EditLocationTest.java
@@ -0,0 +1,11 @@
+package com.CMPUT301W14T13.gpscommentsloggertests.tests25to28;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class EditLocationTest extends ActivityInstrumentationTestCase2<T> {
+
+	public EditLocationTest(String name) {
+		super(name);
+	}
+
+}
---
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentsloggertests/tests25to28/ReplyToCommentTest.java
@@ -0,0 +1,31 @@
+package com.CMPUT301W14T13.gpscommentsloggertests.tests25to28;
+
+import com.CMPUT301W14T13.gpscommentslogger.Comment;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ReplyToCommentTest extends ActivityInstrumentationTestCase2<Comment> {
+
+	public ReplyToCommentTest(String name) {
+		super(Comment.class);
+	}
+	
+	public void testCommentReply () {
+		//Goal: create comment, reply to it, check that reply is a child of the comment
+		
+		String replyText = "My reply";
+		
+		// Create new comment
+		Comment comment = Comment();
+		
+		// Reply to comment
+		comment.reply(replyText);
+		
+		// Check last child
+		assertEquals("Comment reply should appear as latest comment child", replyText,
+				comment.getChildren.getPosition(getChildren.lenghth()));
+		
+		
+	}
+
+}
