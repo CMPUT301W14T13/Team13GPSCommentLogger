eac3bd56c41aa8023c2fd2c37b6d598b9d51d09a
arweber1 <arweber@ualberta.ca>
2014-02-19 17:01:05 -0700

    GetLocationTestCompilable

--- GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/tests25to28/GetLocationTest.java
+++ GPSCommentLoggerTest/src/com/CMPUT301W14T13/gpscommentloggertests/tests25to28/GetLocationTest.java
@@ -1,26 +1,32 @@
 package com.CMPUT301W14T13.gpscommentloggertests.tests25to28;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.CMPUT301W14T13.gpscommentslogger.LocationSelectionView
+import com.CMPUT301W14T13.gpscommentlogger.DebugActivity;
+//import com.CMPUT301W14T13.gpscommentslogger.LocationSelectionView
+import com.CMPUT301W14T13.gpscommentlogger.view.LocationSelectionView;
 
 @SuppressLint("NewApi")
-public class GetLocationTest extends ActivityInstrumentationTestCase2<LocationSelectionView> {
+public class GetLocationTest extends ActivityInstrumentationTestCase2<DebugActivity> {
 
 	public GetLocationTest(String name) {
-		super(LocationSelectionView.class);
+		super(DebugActivity.class);
 	}
 	
 	public void testCurrentLocation () {
 		// Goal: Get location locally in the test file, compare against LocationSelectionView.
 		
-		LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);         
+		LocationManager locManager = (LocationManager)LocationSelectionView.getSystemService(Context.LOCATION_SERVICE);         
 
+		
 		// For locationListener, there is the option of implementing it here,
 		// or using the one from LocationSelectionView.
+		LocationListener locationListener = LocationSelectionView.getLocationListener();
 	    locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000L,500.0f, locationListener);
 	    Location location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 	    
@@ -29,6 +35,8 @@ public class GetLocationTest extends ActivityInstrumentationTestCase2<LocationSe
 	    double longitude=0;
 	    latitude = location.getLatitude();
 	    longitude = location.getLongitude();
+	    
+	    
 	    
 	    // Test latitude
 	    assertEquals("Latitudes should be equal", LocationSelectionView.getLocation().getLatitude(), latitude);
