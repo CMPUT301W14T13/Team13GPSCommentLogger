8385db64ef85ac521b435b3abfe8ac945fd06a2b
kokkenshodai <kyomaru@ualberta.ca>
2014-03-01 17:01:45 -0700

    Added search functionality to serverController

---
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/model/ElasticSearchHits.java
@@ -0,0 +1,15 @@
+package com.CMPUT301W14T13.gpscommentlogger.model;
+
+import java.util.Collection;
+
+public class ElasticSearchHits<T> {
+    int total;
+    double max_score;
+    Collection<ElasticSearchResponse<T>> hits;
+    public Collection<ElasticSearchResponse<T>> getHits() {
+        return hits;
+    }
+    public String toString() {
+        return (super.toString()+","+total+","+max_score+","+hits);
+    }
+}
