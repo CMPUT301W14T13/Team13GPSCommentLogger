7c974c0e15e951154da041c241e0e3640718fb5f
Navjeet Dhaliwal <navjeet.d@gmail.com>
2014-03-14 08:00:08 -0600

    almost done

--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/view/CreateTopicActivity.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/view/CreateTopicActivity.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,15 +24,20 @@ public class CreateTopicActivity extends Activity{
 	private String title;
 	private String commentText;
 	private Location location;
+	
 	private SubmissionController controller;
 	private Topic topic;
+	private static final int REQUEST_CODE = 1;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_topic);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        
+        LocationManager lm;
+        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        String provider = lm.getProvider(LocationManager.GPS_PROVIDER).toString();
+        location = lm.getLastKnownLocation(provider);
     }
     
 	//extract the information that the user has entered
@@ -63,8 +69,21 @@ public class CreateTopicActivity extends Activity{
 		
 	}
 	public void openMap(View view) {
-		Intent topic = new Intent(this, MapViewActivity.class);
-		startActivityForResult(topic, 1);
+		Intent map = new Intent(this, MapViewActivity.class);
+		map.putExtra("lat", 48.13);
+		map.putExtra("lon", -1.63);
+		startActivityForResult(map, REQUEST_CODE);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_CODE){
+			if (resultCode == RESULT_OK){
+				double latitude = data.getDoubleExtra("lat", location.getLatitude());
+				double longitude = data.getDoubleExtra("lon", location.getLongitude());
+				location.setLongitude(longitude);
+				location.setLatitude(latitude);
+			}	
+		}
 	}
 	
 	public void submitTopic(View v){
--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/view/MapViewActivity.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/view/MapViewActivity.java
@@ -1,5 +1,8 @@
 package com.CMPUT301W14T13.gpscommentlogger.view;
 
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.bonuspack.overlays.MapEventsOverlay;
+import org.osmdroid.bonuspack.overlays.MapEventsReceiver;
 import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
@@ -7,6 +10,7 @@ import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MotionEvent;
@@ -21,49 +25,74 @@ public class MapViewActivity extends Activity {
 
 	private MapController mapController;
     private MapView mapView;
+    private GeoPoint returnPoint;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_view);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        
+        Intent intent = getIntent();
+        double lat = intent.getDoubleExtra("lat", 53.5333);
+        double lon = intent.getDoubleExtra("lon",113.5000);
+        GeoPoint center = new GeoPoint(lat, lon);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
-        mapView.setClickable(true);
+        mapView.setClickable(false);
         mapController = (MapController) mapView.getController();
         mapController.setZoom(15);
-        
         //this point in future will be taken from previous activity
-        GeoPoint center = new GeoPoint(48.13, -1.63);
+        
         mapController.setCenter(center);
-
-      
-        mapView.setOnTouchListener( new View.OnTouchListener() {
-			
+        
+        final int markerIndex =setMarker(center);
+        
+        MapEventsReceiver receiver = new MapEventsReceiver() {
+			int mIndex = markerIndex;
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				GeoPoint point = (GeoPoint) mapView.getProjection().fromPixels(event.getX(), event.getY());
-		        mapView.getOverlays().clear();
-				Marker marker = new Marker(mapView);
-		        marker.setPosition(point);
-		        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-		        mapView.getOverlays().add(marker);
-		        mapView.invalidate();
+			public boolean singleTapUpHelper(IGeoPoint arg0) {
+				GeoPoint point = (GeoPoint) arg0;
+				returnPoint = point;
+				mapView.getOverlays().remove(mIndex);
+				mIndex = setMarker(point);
 				return true;
 			}
-		});
+			
+			@Override
+			public boolean longPressHelper(IGeoPoint arg0) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		};
+		
+        MapEventsOverlay mEvent = new MapEventsOverlay(this, receiver);
+        mapView.getOverlays().add(mEvent);
         
         
 	}
 	
 
+	protected  int setMarker(GeoPoint point){
+		Marker marker = new Marker(mapView);
+        marker.setPosition(point);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mapView.getOverlays().add(marker);
+        mapView.invalidate();
+        
+        return  mapView.getOverlays().indexOf(marker);
+	}
 	
 	protected boolean isRouteDisplayed() {
 	    return false;
 	}
 	
+	public void submitLocation(){
+		Intent result = new Intent();
+		result.putExtra("lat", returnPoint.getLatitude());
+		result.putExtra("lon", returnPoint.getLongitude());
+		setResult(RESULT_OK, result);		
+	}
 	
 }
