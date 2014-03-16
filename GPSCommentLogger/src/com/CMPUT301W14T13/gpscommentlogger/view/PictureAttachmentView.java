469f0dc979df2acc32d36e28a612fd75d5b4f5af
Monir Imamverdi <monir.imamverdi@gmail.com>
2014-03-16 16:49:42 -0600

    Image attachment testing with app started
    Up to this point I've been testing image attachment on a separate
    project.

--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/view/PictureAttachmentView.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/view/PictureAttachmentView.java
@@ -1,43 +1,40 @@
 package com.CMPUT301W14T13.gpscommentlogger.view;
 
+import com.CMPUT301W14T13.gpscommentlogger.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
 
 /**
  * @uml.dependency   supplier="com.CMPUT301W14T13.gpscommentlogger.view.CommentMakingView"
  */
-public class PictureAttachmentView
+public class PictureAttachmentView extends Activity
 {
 
-	private void getPicture(){
+	// Reference: http://stackoverflow.com/questions/2169649/get-pick-an-image-from-androids-built-in-gallery-app-programmatically
+	
+	private static final int PICK_FROM_FILE = 1;
+	private String selectedImagePath;
+	
+	public void onCreate(Bundle savedInstaceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_attachment_view);
+		
+		((Button) findViewById(R.id.pick_image)).setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				
+			}
+		});
 		
 	}
-	/** 
-	 * @uml.property name="c"
-	 * @uml.associationEnd multiplicity="(1 1)" inverse="i:com.CMPUT301W14T13.gpscommentlogger.view.CommentMakingView"
-	 */
-	private CreateCommentActivity c = new com.CMPUT301W14T13.gpscommentlogger.view.CreateCommentActivity();
-
-	/** 
-	 * Getter of the property <tt>c</tt>
-	 * @return  Returns the c.
-	 * @uml.property  name="c"
-	 */
-	public CreateCommentActivity getC()
-	
-	
-	{
-		return c;
-	}
-
-	/** 
-	 * Setter of the property <tt>c</tt>
-	 * @param c  The c to set.
-	 * @uml.property  name="c"
-	 */
-	public void setC(CreateCommentActivity c)
-	
-	
-	{
-		this.c = c;
-	}
-
 }
