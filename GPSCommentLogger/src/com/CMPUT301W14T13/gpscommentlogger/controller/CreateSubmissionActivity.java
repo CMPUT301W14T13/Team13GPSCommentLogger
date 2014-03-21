640272da4c074b71f53c0f5e30878fa29870de9a
Navjeet <navjeet.d@gmail.com>
2014-03-21 16:16:17 -0600

    CreateSubmissionActvity now checks network status when you click location button

--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/CreateSubmissionActivity.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/CreateSubmissionActivity.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.location.Location;
@@ -261,16 +263,36 @@ public class CreateSubmissionActivity extends Activity{
 		startActivityForResult(Intent.createChooser(intent, "Gallery"), PICK_FROM_FILE);
 	}
 	/**
-	 * Opens the MapViewActivity once the location button is clicked
-	 * passes it a latitude and longitude from the current gps location
-	 * to be used to set the map screen.
+	 * Once location button is clicked we check if our user is online, if so we pop up a map 
+	 * to edit location otherwise we open a dialog fragment for offline location editing
 	 * @param view
 	 */
 	public void openMap(View view) {
+		if(isOnline()){
 		Intent map = new Intent(this, MapViewActivity.class);
 		map.putExtra("lat", gpsLocation.getLatitude());
 		map.putExtra("lon", gpsLocation.getLongitude());
 		startActivityForResult(map, REQUEST_CODE);
+		} else {
+			
+			new AlertDialog.Builder(this)
+		    .setTitle("Location")
+		    .setMessage("Please enter your desired location")
+		    .setView(view)
+		    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int which) { 
+		            // continue with delete
+		        }
+		     })
+		    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int which) { 
+		            // do nothing
+		        }
+		     })
+		    .setIcon(R.drawable.attach)
+		     .show();
+			
+		}
 	}
 	/**
 	 * extracts a longitude and latitude from MapViewActivity to be used
@@ -504,6 +526,15 @@ public class CreateSubmissionActivity extends Activity{
 	public void onDestroy(){
 		super.onDestroy();
 		lm.removeUpdates(ll);
+	}
+	
+	public boolean isOnline() {
+	    ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+	    if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+	        return true;
+	    }
+	    return false;
 	}
 
 }
