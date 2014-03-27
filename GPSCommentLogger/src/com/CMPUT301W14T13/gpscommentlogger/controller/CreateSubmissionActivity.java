3a5099e595f8541ac94c42571ec63224100cb92e
Navjeet <navjeet.d@gmail.com>
2014-03-27 15:42:27 -0600

    nothing changed

--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/CreateSubmissionActivity.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/CreateSubmissionActivity.java
@@ -231,7 +231,6 @@ public class CreateSubmissionActivity extends Activity{
 	public void openMap(View view) {
 		if(isOnline()){
 			Intent map = new Intent(this, MapViewActivity.class);
-			Log.d("CreateSubmissionActivity", locationGetter.getLocation().toString());
 			map.putExtra("lat", locationGetter.getLocation().getLatitude()); 
 			map.putExtra("lon", locationGetter.getLocation().getLongitude());
 			map.putExtra("canSetMarker", 1);// for editing  location
