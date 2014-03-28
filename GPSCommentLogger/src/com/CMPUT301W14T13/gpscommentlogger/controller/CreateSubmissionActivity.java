cc979acfcef167d85350818dffd6c7f7f1161fc9
Navjeet Dhaliwal <navjeet.d@gmail.com>
2014-03-28 14:35:56 -0600

    Revert "null pointer came from not getting gps locations from GPS provider"
    This reverts commit 79f4b86c822bfc64e4f5e6c957104ebd6ad6d701.

--- GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/CreateSubmissionActivity.java
+++ GPSCommentLogger/src/com/CMPUT301W14T13/gpscommentlogger/controller/CreateSubmissionActivity.java
@@ -253,7 +253,6 @@ public class CreateSubmissionActivity extends Activity{
 							double latitude = Double.parseDouble(text.getText().toString().trim());
 							text = (EditText) dialogView.findViewById(R.id.offlineLongitude);
 							double longitude = Double.parseDouble(text.getText().toString().trim());
-							location = locationGetter.getLocation();
 							location.setLatitude(latitude);
 							location.setLongitude(longitude);
 							
@@ -286,7 +285,7 @@ public class CreateSubmissionActivity extends Activity{
 			if (resultCode == RESULT_OK){
 				double latitude = data.getDoubleExtra("lat", locationGetter.getLocation().getLatitude());
 				double longitude = data.getDoubleExtra("lon", locationGetter.getLocation().getLongitude());
-				location = locationGetter.getLocation();
+
 				location.setLongitude(longitude);
 				location.setLatitude(latitude);
 
