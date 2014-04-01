8ddba6b7dab9fb1ef7de3be3bfaa2298f6e49926
Mike Nicholson <mjnichol@ualberta.ca>
2014-03-31 23:28:25 -0600

    Integrated controller into view

--- RefactoredProject/RefactoredProject/src/cmput301w14t13/project/views/FavouritesView.java
+++ RefactoredProject/RefactoredProject/src/cmput301w14t13/project/views/FavouritesView.java
@@ -1,19 +1,22 @@
 package cmput301w14t13.project.views;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ListView;
-import android.widget.TextView;
 import cmput301w14t13.project.R;
+import cmput301w14t13.project.auxilliary.adapters.CustomAdapter;
 import cmput301w14t13.project.auxilliary.interfaces.UpdateInterface;
 import cmput301w14t13.project.auxilliary.interfaces.UpdateRank;
+import cmput301w14t13.project.controllers.FavouritesViewController;
 
 public class FavouritesView extends Activity implements UpdateInterface
 {
 	
 	private ListView favouritesListview;
+	private CustomAdapter displayAdapter; //adapter to display the topics
+	private Menu menu; //A reference to the options menu
+	protected FavouritesViewController controller = new FavouritesViewController(this);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
