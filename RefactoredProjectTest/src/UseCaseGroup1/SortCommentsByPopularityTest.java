a1ca1aca8c69b6195ad130816616f4b5d568e897
Mike Nicholson <mjnichol@ualberta.ca>
2014-04-01 19:49:19 -0600

    Properly configured the testing suite. It was poorly constructed before.

--- RefactoredProjectTest/src/UseCaseGroup1/SortCommentsByPopularityTest.java
+++ RefactoredProjectTest/src/UseCaseGroup1/SortCommentsByPopularityTest.java
@@ -1,8 +1,10 @@
 package UseCaseGroup1;
 
+import android.annotation.SuppressLint;
 import android.test.ActivityInstrumentationTestCase2;
 import cmput301w14t13.project.views.HomeView;
 
+@SuppressLint("NewApi")
 public class SortCommentsByPopularityTest extends
 ActivityInstrumentationTestCase2<HomeView> {
 
--- RefactoredProjectTest/src/UseCaseGroup1/SortCommentsByTimeTest.java
+++ RefactoredProjectTest/src/UseCaseGroup1/SortCommentsByTimeTest.java
@@ -1,8 +1,10 @@
 package UseCaseGroup1;
 
+import android.annotation.SuppressLint;
 import android.test.ActivityInstrumentationTestCase2;
 import cmput301w14t13.project.views.HomeView;
 
+@SuppressLint("NewApi")
 public class SortCommentsByTimeTest extends
 ActivityInstrumentationTestCase2<HomeView> {
 
