6f59d6203d145b0a1c3abbfd9dbfc6118c50fce1
DevMomo <imamverd@cs.ualberta.ca>
2014-04-04 13:36:10 -0600

    Started BrowseCommentRepliesTest

--- RefactoredProjectTest/src/usecasegroup3/BrowseCommentRepliesTest.java
+++ RefactoredProjectTest/src/usecasegroup3/BrowseCommentRepliesTest.java
@@ -1,15 +1,28 @@
 package usecasegroup3;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import cmput301w14t13.project.views.HomeView;
+import cmput301w14t13.project.views.TopicView;
 
 @SuppressLint("NewApi")
 public class BrowseCommentRepliesTest extends
 ActivityInstrumentationTestCase2<HomeView> {
+	
+	TopicView topicView;
+	Intent intent;
 
 	public BrowseCommentRepliesTest() {
 		super(HomeView.class);
 	}
+	
+	public void setUp() throws Exception {
+		super.setUp();
+		intent = new Intent();
+		setActivityIntent(intent);
+	}
+	
+	
 
 }
