1d14abb81bd58c48b00ebeb1b0ace57449966283
Mike Nicholson <mjnichol@ualberta.ca>
2014-04-04 12:09:59 -0600

    working with Josh to debug edit comment test

--- RefactoredProjectTest/src/usecasegroup2/integration/EditCommentIntegrationTest.java
+++ RefactoredProjectTest/src/usecasegroup2/integration/EditCommentIntegrationTest.java
@@ -8,6 +8,7 @@ import android.annotation.SuppressLint;
 import android.app.Instrumentation;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,10 +28,10 @@ ActivityInstrumentationTestCase2<HomeView> {
 
 	HomeView homeView;
 	TopicView topicView;
-	
+
 	CreateSubmissionController create;
 	HomeViewController homeControl;
-	
+
 	TopicViewController edit;
 	Intent intent;
 	CommentTree ct = CommentTree.getInstance();
@@ -46,7 +47,19 @@ ActivityInstrumentationTestCase2<HomeView> {
 		setActivityIntent(intent);
 
 	}
-	// write the integration test for editing a comment! 
+	
+	@UiThreadTest
+	public void testHomeViewUpdate() {
+		homeView = getActivity();
+		homeView.update();
+		ListView topicList = (ListView) homeView.findViewById(cmput301w14t13.project.R.id.topic_listview);
+		assertNotNull(topicList);
+		topicList.performItemClick(
+				topicList.getAdapter().getView(0, null, null), 0, 0);
+
+	}
+	
+	// write the integration test for editing a comment!
 	public void testEditTopic() throws Throwable{
 		homeView = getActivity();
 		assertNotNull(homeView);
@@ -79,7 +92,7 @@ ActivityInstrumentationTestCase2<HomeView> {
 				title.setText("title");
 				username.setText("User1");
 				commentText.setText("text");
-				
+
 				submitButton.performClick();
 
 				// give the topic a second to get pushed to the server 
@@ -88,35 +101,56 @@ ActivityInstrumentationTestCase2<HomeView> {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
+
 				/* make sure we have entered the CreateSubmissionController Activity */
-				
+
 				ArrayList<CommentTreeElement> topics = SortFunctions.sortByNewest(ct.getChildren(homeView));
 				CommentTreeElement topic = topics.get(0);
 
 				assertEquals("Titles not the same", topic.getTitle(), title.getText().toString() );
 				assertEquals("Usernames not the same", topic.getUsername(), username.getText().toString());
 				assertEquals("Texts not the same", topic.getCommentText(), commentText.getText().toString());
-		
+
+
+
+
+			}
+		});
+
+		/* Now click the comment and edit it */
+		// Instrumentation.ActivityMonitor homeViewMonitor = getInstrumentation().addMonitor(HomeViewController.class.getName(), null , false);
+		// give the topic a second to get pushed to the server 
+
+		/* stop the runnable here */
+
+
+
+
+		runTestOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				/* I don't think I am correctly in the activity I think I am */
+				homeView = getActivity();
+				homeView.update();
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
-				/* I don't think I am correctly in the activity I think I am */
 				ListView topicList = (ListView) homeView.findViewById(cmput301w14t13.project.R.id.topic_listview);
 				assertNotNull(topicList);
-				
+		
 				/* click the newly created topic */
 				topicList.performItemClick(
-					    topicList.getAdapter().getView(0, null, null), 0, 0);
-				
+						topicList.getAdapter().getView(0, null, null), 0, 0);
+
 				/* you are now in the new Activity, so change the parameters */
+/*
 				title.setText("title_new");
 				username.setText("User2");
 				commentText.setText("text_new");
-				
+
 				submitButton.performClick();
 				topics = SortFunctions.sortByNewest(ct.getChildren(homeView));
 				topics.get(0);
@@ -124,19 +158,10 @@ ActivityInstrumentationTestCase2<HomeView> {
 				assertEquals("Titles not the same", topic.getTitle(), title.getText().toString() );
 				assertEquals("Usernames not the same", topic.getUsername(), username.getText().toString());
 				assertEquals("Texts not the same", topic.getCommentText(), commentText.getText().toString());
-				
-				
+*/
 			}
 		});
-		
-		/* Now click the comment and edit it */
-		// Instrumentation.ActivityMonitor homeViewMonitor = getInstrumentation().addMonitor(HomeViewController.class.getName(), null , false);
-		// give the topic a second to get pushed to the server 
 
-		
-
-		
-		
 
 	}
 }
