ee904b2c773a15ce982228b122628300559ade5d
Monir Imamverdi <monir.imamverdi@gmail.com>
2014-04-06 20:10:36 -0600

    Null Pointer on Edit Fixes

--- RefactoredProject/RefactoredProject/src/cmput301w14t13/project/views/submissions/EditTopicSubmissionView.java
+++ RefactoredProject/RefactoredProject/src/cmput301w14t13/project/views/submissions/EditTopicSubmissionView.java
@@ -1,13 +1,15 @@
 package cmput301w14t13.project.views.submissions;
 
-import cmput301w14t13.project.controllers.submissions.EditTopicSubmissionController;
 import android.os.Bundle;
+import cmput301w14t13.project.R;
+import cmput301w14t13.project.controllers.submissions.EditTopicSubmissionController;
 
 public class EditTopicSubmissionView extends TopicSubmissionView {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);		
+
 		controller = new EditTopicSubmissionController(this);
 
 		controller.initialize();
